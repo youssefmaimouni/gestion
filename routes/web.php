035f151d4b915c0fd3d815c71a53c094b5cee9b4
youssef Maimouni <youssefmaimouni03@gmail.com>
2024-06-27 09:56:43 +0100

    "Updated navigation bar and layout files, added responsive design elements, and modified routes"

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/marchandise', [marchandise::class, 'index'])->name('marchandise');
+    Route::get('/marchandise/modif', [marchandise::class, 'index'])->name('marchandise.modif');
+    Route::post('/marchandise/update', [marchandise::class, 'update'])->name('marchandise.update');
 });
 
 require __DIR__.'/auth.php';
