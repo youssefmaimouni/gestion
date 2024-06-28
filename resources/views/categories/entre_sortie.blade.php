36519dc57d757dc83ba9f124a688f59a1d8f88aa
ihssane imouchi <ihssane.imouchi-etu@etu.univh2c.ma>
2024-06-28 16:00:36 +0100

    hfvzj

--- resources/views/categories/entre_sortie.blade.php
+++ resources/views/categories/entre_sortie.blade.php
@@ -1,21 +1,23 @@
 <x-nav-bar>
-    <div class=" flex">
-        <p class="text-2xl w-2/3 m-3 pl-6 underline underline-offset-4">categories</p>
-        <p class="text-xl w-1/3  m-3 pl-6"><a href="{{ route('categories.create') }}" class="text-blue-600 hover:text-blue-900">Ajouter categorie</a> </p>
-    </div>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          
 
-            @foreach($categories as $categorie)
-            <div class=" overflow-hidden shadow-sm sm:rounded-lg flex" >
-               <a href="{{ route('categories.entre_sortie', $categorie) }}" class="h-52 w-1/2 bg-slate-400 m-3 flex items-center flex-col   text-center justify-center">
-                {{ $categorie->nom }} 
-               </a>
-               
-            </div>
-            @endforeach
-        </div>
-    </div>
-   
+<div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+    <ul class="flex flex-wrap -mb-px">
+        <li class="me-2">
+            <a href="#" class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Profile</a>
+        </li>
+        <li class="me-2">
+            <a href="#" class="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500" aria-current="page">Dashboard</a>
+        </li>
+        <li class="me-2">
+            <a href="#" class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Settings</a>
+        </li>
+        <li class="me-2">
+            <a href="#" class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Contacts</a>
+        </li>
+        <li>
+            <a class="inline-block p-4 text-gray-400 rounded-t-lg cursor-not-allowed dark:text-gray-500">Disabled</a>
+        </li>
+    </ul>
+    
+</div>
 </x-nav-bar>
