02a732c4c34168226b86e90bc8a831babc76f9c6
youssefmaimouni <135459547+youssefmaimouni@users.noreply.github.com>
2024-07-15 14:49:54 +0100

    "Updated navigation layout: changed responsive navigation menu styles and added background color to settings options"

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -64,7 +64,7 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden bg-white shadow-sm rounded sm:hidden">
+    <div :class="{'block': open, 'hidden': ! open}" class="hidden bg-white w-2/3 h-full shadow-sm rounded sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Accueil') }}
@@ -72,7 +72,7 @@
         </div>
 
         <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
+        <div class="pt-4 pb-1 border-t bg-white border-gray-200">
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
                 <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
